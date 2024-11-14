abd40f3f57b4dc41f9c9d84096ab27050e4692af
nutrism <nutricionistasilviamartins@gmail.com>
2024-11-14 13:46:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ def criar_conexao():
     conn = psycopg2.connect(
         database=url.path[1:],
         user=url.username,
-        password=url.password,   
-
+        password=url.password,
         host=url.hostname,
         port=url.port
     )
